b351d99a1e642fa5339ab5ab0ea4dd2f5221065d
Axel Plasa <107868311+aplasa85@users.noreply.github.com>
2022-09-19 16:01:46 +0200

    Add cards to App.js

--- src/App.js
+++ src/App.js
@@ -1,2 +1,20 @@
-<div>
-  </div>
+import "./App.css";
+import Header from "./components/header/Header";
+import Navigation from "./components/navigation/Navigation";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <main className= "app__main">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+       </main>
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
