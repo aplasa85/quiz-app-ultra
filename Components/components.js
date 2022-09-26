f90e61eee73bc3acb8aabb268638b5fe67f4c6c7
Axel Plasa <107868311+aplasa85@users.noreply.github.com>
2022-09-26 11:07:33 +0200

    Add components folder with card, footer, and header js

---
+++ Components/components.js
@@ -0,0 +1,19 @@
+import App from "../src/App"
+import Components from '../Components/components';
+
+
+function Component(props) {
+return (
+<section>
+    style= {{
+        backgroundColor: backgroundColor,
+        >
+    }}
+    className= "my_component_container"
+    <h2>Hello everybody!</h2>
+</section>
+
+)}
+}
+
+export default Component
--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import "./App.css";
-import Header from "./Components/header/Header";
-import Navigation from "./Components/navigation/Navigation";
 
-function App() {
-  return (
-    <div className="app">
-      <Header />
-      <main className= "app__main">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-       </main>
-      <Navigation />
-    </div>
-  );
-}
+import Components from '../Components/components';
+import './Index.css';
+
+
+
+
 
 export default App;
+console.clear();
+function App() {
+  return (
+  <section></section>
+  <main>
+
+<main>
+<>    
+      <Component backgroundColor= "orange">
+      <Component backgroundColor= "orange">
+      </main>
+      </section>
+    </>
+<main />
+);}g
+}
+export default Components;
+
+useEffect (() => {
+{JSON.strinify ())}, [];
+
+}
+
+
+
+useEffect (() => {
+JSON.parse () localStoragge.getItem()
+}, [];
+
+</main>)
